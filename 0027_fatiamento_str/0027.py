49005e7f226975633257b5f71161509e66208050
m2t8u6 <matheusgsaquino@gmail.com>
2024-07-28 18:27:22 -0300

    att

---
+++ 0027_fatiamento_str/0027.py
@@ -0,0 +1,7 @@
+# fatiamento [i:f:p]
+nome = 'Pedro Jorge'
+print(nome[0:3])
+
+# A função 'len' é  usadaa para contar carcteres
+quantidade = len(nome)
+print(quantidade)
---
+++ 0028_exercicio/0028.py
@@ -0,0 +1,24 @@
+nome = input('Digite o seu nome: ')
+qnt = len(nome)
+idade = input('Digite a sua idade: ')
+
+if nome == ' ':
+    print('Digite um nome valido.')
+    quit()
+elif idade.isalpha():
+    print('Você digitou uma letra ou caractere não númerico.')
+    quit()
+else:
+    idade = int(idade)
+
+print(f'\nSeu nome é: {nome}')
+print(f'Seu nome invertido é: {nome[::-1]}\n')
+
+if ' ' in nome:
+    print('O nome contem espaço.')
+else:
+    print('O nome não Stem espaço.')
+
+print(f'\nSeu nome tem {qnt} caracteres.')
+print(f'\nA primeira letra do seu nome é: {nome[0]}')
+print(f'A ultima letra do seu nome é: {nome[-1]}')
