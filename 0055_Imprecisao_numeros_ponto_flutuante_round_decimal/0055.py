00c1147b2524634b9a5959e43b84325eb8e3653c
m2t8u6 <matheusgsaquino@gmail.com>
2024-08-25 17:53:35 -0300

    split, join, imprecisao

---
+++ 0055_Imprecisao_numeros_ponto_flutuante_round_decimal/0055.py
@@ -0,0 +1,10 @@
+import decimal
+
+numero = 3.1415
+
+print(round(numero, 10))
+
+x = decimal.Decimal('0.9')
+y = decimal.Decimal('0.1415')
+
+print(x + y)
---
+++ 0056_split_join_strip/0056.py
@@ -0,0 +1,11 @@
+# split | cria uma lista sem espaços em branco | é possivel dizer quando dividir a lista
+
+frase = 'Ola, mundo! é um prazer te conhecer. Olha so que, coisa legal!'
+
+lista_frases  = frase.split(',')
+
+print(lista_frases)
+
+f = 'ola, como vai?'
+
+print('-'.join(f))
